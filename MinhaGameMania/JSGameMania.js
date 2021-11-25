e291de583467e7350f5fd7e9063d4cfcb513f2b7
KaduCury <carlosercury@gmail.com>
2021-11-25 16:36:18 -0300

    25/11 pré Angular

--- MinhaGameMania/JSGameMania.js
+++ MinhaGameMania/JSGameMania.js
@@ -15,3 +15,24 @@ function alerta(){
 function addSacola(){
     alert("Seu produto foi incluído na sacola de compras")
 }
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
